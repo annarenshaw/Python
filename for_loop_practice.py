62d19ac10448307da920c9054a4b1991cabe031a
annarenshaw <110431859+annarenshaw@users.noreply.github.com>
2022-08-14 17:42:46 -0500

    Add files via upload

---
+++ for_loop_practice.py
@@ -0,0 +1,14 @@
+for number in range(10):
+    print('Hello')
+    print('hi there')
+
+for number in range(10):
+    print(number)
+
+fav_movies = ['Scream', 'Alien', 'Forgetting Sarah Marshall', 'Black Swan']
+
+for movie in fav_movies:
+    print(movie)
+
+for number in range(40):
+    print((number+1)*2)
---
+++ input_practice.py
@@ -0,0 +1,10 @@
+user_text = input('enter some text: ')
+
+
+upp_or_low = input('Enter 1 to uppercase and 2 to lowercase:')
+
+if upp_or_low == "1":
+    print(user_text.upper())
+else:
+    print(user_text.lower())
+
